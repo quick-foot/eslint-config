5a501bfd50e247df439aee08ac15b1e4ed704177
Christopher <24984762+ChristopherKiss@users.noreply.github.com>
2019-02-25 05:25:18 +1100

    fix: add parser options for es6

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
-    rules: {
-        'array-type': ['error', 'array']
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 6
     }
 };
